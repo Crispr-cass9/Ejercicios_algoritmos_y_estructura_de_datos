2050bced889fc669a2b71051ea48e952c15916fe
Crispr-Cass <cristian.jovani2001@gmail.com>
2024-03-15 12:22:24 +0100

    Finalizados los métodos de tablas hash

--- Cap-7-Tablas-Hash/tabla-hash.py
+++ Cap-7-Tablas-Hash/tabla-hash.py
@@ -27,7 +27,27 @@ def bernstein(string):
     
     return h
 
+
 def agregar(tabla, dato):
     posicion = funcion_hash(dato, len(tabla))
     if tabla[posicion] is None:
         tabla[posicion] = Lista()
+    insertar(tabla[posicion], dato) #Esto proviene de los métodos de listas enlazadas
+
+
+def buscar(tabla, buscado):
+    pos = None
+    posicion = funcion_hash(buscado, len(tabla))
+    if tabla[posicion is not None]:
+        pos = busqueda(tabla[posicion], buscado)
+    return pos
+
+def quitar(tabla, dato):
+    dato = None
+    posicion = funcion_hash(dato, len(tabla))
+    if tabla[posicion] is not None:
+        dato = eliminar(tabla[posicion], dato) #Recordar que tabla[posicion] es una lista enlazada
+        if lista_vacia(tabla[posicion]):
+            tabla[posicion] = None
+    return dato
+
